d10ece3afeaca0305a1b6882cdaeb8c7314f0481
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2024-06-04 11:05:57 -0400

    Created new account files for Raymond and Marcus
    n word

--- App.java
+++ App.java
@@ -3,6 +3,6 @@ import java.io.*;
 public class App {
     public static void main(String[] args) throws IOException {
         GUI gui = new GUI();
-        gui.show(); 
+        gui.show();
     }
 }
