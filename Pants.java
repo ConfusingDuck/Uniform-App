ac6c3bdd1e950667e8a6e1207da44569997a0a9d
victorgbanica <victorgabrielbanica@gmail.com>
2024-06-05 11:43:15 -0400

    Updated pants

---
+++ Pants.java
@@ -0,0 +1,11 @@
+public class Pants extends Clothing {
+
+    public Pants(){
+        super();
+    }
+
+    public Pants(double p, String c, String g, String s) {
+        super(p,c,g,s);
+    }
+
+}
