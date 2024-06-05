4ab3aa7532f3d48ae24580e8c42ae6a2a93330b3
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2024-06-05 11:41:27 -0400

    Added basid GUI after initial login screen

--- App.java
+++ App.java
@@ -3,16 +3,15 @@ import java.lang.Thread;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        //Displays splash screen for 5 seconds and then closes it
+        // Displays splash screen for 5 seconds and then closes it
         try {
             SplashScreen splashScreen = new SplashScreen();
             splashScreen.show();
             Thread.sleep(2500);
             splashScreen.close();
+        } catch (Exception e) {
         }
-        catch (Exception e) {
-        }
-       
+
         GUI gui = new GUI();
         gui.show();
     }
--- GUI2.java
+++ GUI2.java
@@ -1,4 +1,44 @@
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.Color;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
 
 public class GUI2 extends javax.swing.JFrame {
-    
+
+    private JFrame frame;
+    private JPanel panel;
+
+    public GUI2() {
+
+        frame = new JFrame();
+        panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panel.setLayout(new GridBagLayout());
+        // c determines where an object is within the gridBagLayout
+        GridBagConstraints c = new GridBagConstraints();
+        // c is given insets to pad objects in the layout
+        c.insets = new Insets(0, 5, 20, 0);
+
+        // Automatically close program when frame is closed.
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Main Screen");
+        frame.setSize(1500, 1000);
+        frame.setLocationRelativeTo(null);
+        frame.add(panel);
+
+    }
+
 }
