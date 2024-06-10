cb7499dfc1412b0d29843d423ed94711d22bb79d
victorgbanica <victorgabrielbanica@gmail.com>
2024-06-10 19:59:37 -0400

    Now returns an arraylist of clothing when searching by filter

--- FileEditor.java
+++ FileEditor.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class FileEditor {
     
@@ -32,21 +33,26 @@ public class FileEditor {
         return clothing;
     }
 
-    public static Clothing retreiveByFilter(String filter) {
+    public static ArrayList<Clothing> retreiveByFilter(String filter) {
         String line = " ";
+        ArrayList<Clothing> clothes = new ArrayList<Clothing>();
         try{
             FileReader fr = new FileReader("clothingList.txt");
             BufferedReader br = new BufferedReader(fr);
             while ((line = br.readLine()) != null) {
                 if (line.contains(filter)) {
                     Clothing clothing = new Clothing(line);
-                    return clothing;
+                    clothes.add(clothing);
                 }
+            }
+            if (clothes.size() > 0) {
+                return clothes;
             }
         }
         catch(IOException e) {
         }
         Clothing clothing = new Clothing();
-        return clothing;
+        clothes.add(clothing);
+        return clothes;
     }
 }
