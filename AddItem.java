86cb1b016c1e6b45676aeee4b5e5411ce9dfebd9
Marcus Chuong <49174541+toenailMuncher@users.noreply.github.com>
2024-06-10 13:45:55 -0400

    changed everything bacjk

--- AddItem.java
+++ AddItem.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.NumberFormat;
 import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
@@ -14,9 +13,8 @@ import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.JTextField;
-import javax.swing.filechooser.FileSystemView;
+import javax.swing.JPasswordField;
 import javax.swing.JOptionPane;
-import javax.swing.JFileChooser;
 
 public class AddItem extends JFrame {
     private JFrame frame;
@@ -34,29 +32,12 @@ public class AddItem extends JFrame {
     private JComboBox cmbSize;
     private JComboBox cmbItem;
 
-    private JFileChooser fcUpload;
-    private JButton btnUpload;
-    private JButton btnAdd;
-
-    private double price;
-    private String condition;
-    private String gender;
-    private String size;
-    private String item;
-    private String imagePath;
-    private String strPrice;
-    private boolean validPrice;
-
-    //Make lists of all available options for each combo box
-    private final String[] conditions = {" ", "Heavily Worn", "Moderately Worn", "Lightly Worn", "New"};
-    private final String[] genders = {" ", "Men's", "Women's"};
-    private final String[] sizes = {" ", "XS", "S", "M", "L", "XL"};
-    private final String[] items = {" ", "Short-Sleeve Polo", "Long-Sleeve Polo", "Sweater", "Pants"};
+    private final String[] conditions = { " ", "Heavily Worn", "Moderately Worn", "Lightly Worn", "New" };
+    private final String[] genders = { " ", "Men's", "Women's" };
+    private final String[] sizes = { " ", "XS", "S", "M", "L", "XL" };
+    private final String[] items = { " ", "Short-Sleeve Polo", "Long-Sleeve Polo", "Sweater", "Pants" };
 
     public AddItem(User user) {
-        imagePath = " ";
-        strPrice = "";
-        validPrice = true;
         frame = new JFrame();
         panel = new JPanel();
 
@@ -74,7 +55,6 @@ public class AddItem extends JFrame {
         frame.setLocationRelativeTo(null);
         frame.add(panel);
 
-        //Initialize all the labels and boxes
         lblItem = new JLabel("Pick the type of item:");
         lblPrice = new JLabel("Set Your Price: $");
         lblCondition = new JLabel("Condition:");
@@ -83,127 +63,45 @@ public class AddItem extends JFrame {
 
         txtPrice = new JTextField(5);
         cmbCondition = new JComboBox<>(conditions);
-        cmbGender= new JComboBox<>(genders);
+        cmbGender = new JComboBox<>(genders);
         cmbSize = new JComboBox<>(sizes);
         cmbItem = new JComboBox<>(items);
 
-        fcUpload = new JFileChooser();
-        btnUpload = new JButton("Upload Picture");
-        btnAdd = new JButton("Add Item");
-
-        //Add all the labels and boxes to the panel.
-        c.anchor = GridBagConstraints.EAST;
-        c.weightx = 1;
-        c.insets = new Insets(5, 5, 5, 5);
+        // Add all the labels and boxes to the panel.
         c.gridx = 0;
         c.gridy = 0;
-        panel.add(lblItem, c);
+        panel.add(lblPrice, c);
         c.gridy = 1;
         panel.add(lblCondition, c);
         c.gridy = 2;
         panel.add(lblGender, c);
         c.gridy = 3;
         panel.add(lblSize, c);
-        c.gridy = 4;
-        panel.add(lblPrice, c);
-        c.gridy = 5;
-        panel.add(btnUpload, c);
-
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        c.anchor = GridBagConstraints.WEST;
         c.gridx = 1;
-        c.gridy = 4;
-        panel.add(txtPrice, c);
-        c.gridy = 3;
         panel.add(cmbSize, c);
         c.gridy = 2;
         panel.add(cmbGender, c);
         c.gridy = 1;
         panel.add(cmbCondition, c);
         c.gridy = 0;
-        panel.add(cmbItem, c);
-        c.gridy = 5;
-        panel.add(btnAdd, c);
+        panel.add(txtPrice, c);
 
-
-        //Give each label and box a font
         lblPrice.setFont(new Font("Sans-serif", Font.PLAIN, 16));
         lblCondition.setFont(new Font("Sans-serif", Font.PLAIN, 16));
         lblGender.setFont(new Font("Sans-serif", Font.PLAIN, 16));
         lblSize.setFont(new Font("Sans-serif", Font.PLAIN, 16));
-        lblItem.setFont(new Font("Sans-serif", Font.PLAIN, 16));
 
         txtPrice.setFont(new Font("Sans-serif", Font.PLAIN, 16));
         cmbCondition.setFont(new Font("Sans-serif", Font.PLAIN, 16));
         cmbGender.setFont(new Font("Sans-serif", Font.PLAIN, 16));
         cmbSize.setFont(new Font("Sans-serif", Font.PLAIN, 16));
-        cmbItem.setFont(new Font("Sans-serif", Font.PLAIN, 16));
-
-        btnAdd.setFont(new Font("Sans-serif", Font.PLAIN, 16));
-        btnUpload.setFont(new Font("Sans-serif", Font.PLAIN, 16));
-
-        //Add an actionListener for the upload picture button
-        btnUpload.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //Opens the computer's file directory so user can browse
-                fcUpload = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-                int x = fcUpload.showOpenDialog(null);
-                //If the user selects an image, then the file path is obtained and stored in imagePath
-                if (x == JFileChooser.APPROVE_OPTION) {
-                    imagePath = fcUpload.getSelectedFile().getAbsolutePath();
-                }
-                //Otherwise, nothing happens and the window closes
-                else {}
-            }
-        });
-
-        //Add an actionListener for the button to add a new item
-        btnAdd.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                strPrice = txtPrice.getText();
-                //Check the price text box to ensure that only digits and decimal points can be added
-                validPrice = true;
-                for (int i = 0; i < strPrice.length(); i++) {
-                    if ((int) strPrice.charAt(i) >= 48 && (int) strPrice.charAt(i) <= 57 || strPrice.charAt(i) == 46) {
-                    }
-                    else {
-                        //Otherwise, price is false and the user is notified
-                        validPrice = false;
-                        JOptionPane.showMessageDialog(frame, "Invalid price.");
-                        break;
-                    }
-                }
-                if (validPrice) {
-                    //If the string only contains digits and a decimal point, then the price can be made a double
-                    price = Double.valueOf(strPrice);
-                }
-                //Gather all the specifics from the user's entry
-                condition = cmbCondition.getSelectedItem().toString();
-                gender = cmbGender.getSelectedItem().toString();
-                size = cmbSize.getSelectedItem().toString();
-                item = cmbItem.getSelectedItem().toString();
-                //If the inputted fields are not spaces (default value), then the submission is valid
-                if(condition.equals(" ") || gender.equals(" ") || size.equals(" ") || item.equals(" ") || validPrice == false) {
-                    JOptionPane.showMessageDialog(frame, "Invalid submission.");
-                }
-                else {
-                    user.setClothingItem(new Clothing(item, condition, price, imagePath, size));
-                    close();
-                }
-            }
-        });
-
 
     }
 
-    /*This method makes the pane visible */
     public void show() {
         frame.setVisible(true);
     }
 
-    /*This method closes the pane */
     public void close() {
         frame.setVisible(false);
     }
